33931698580af133f043010a8828d83ecb26c3c0
AhmedYeasin <ahmetchatgami@gmail.com>
2026-02-04 12:36:36 +0600

    map added

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import { PosAnimation } from "leaflet";
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useLoaderData } from "react-router";
 
 const Coverage = () => {
-    return (
-        <div>
-            Coverage page
-        </div>
-    );
+  const position = [23.685, 90.3563];
+  const serviceCenters = useLoaderData();
+  return (
+    <div>
+      <h2>We are available in 64 district</h2>
+      <div>
+        <form>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" className="grow" placeholder="Search" />
+           <button className="bg-primary p-2 rounded -mx-3">Search</button>
+          </label>
+        </form>
+      </div>
+
+      <div className="w-full h-[800px]">
+        <MapContainer
+          center={position}
+          zoom={8}
+          scrollWheelZoom={false}
+          className="h-[800px]"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {serviceCenters.map((center, index) => (
+            <Marker key={index} position={[center.latitude, center.longitude]}>
+              <Popup>
+                <strong>{center.district}</strong> <br /> Service Area:{" "}
+                {center.covered_area.join(", ")}
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Coverage;
